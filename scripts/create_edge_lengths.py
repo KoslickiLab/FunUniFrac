c338284cbd149f0a449340d8fd561a92af993097
dkoslicki <dmkoslicki@gmail.com>
2022-09-27 22:40:41 -0400

    randomized algorithm approach for #2

--- scripts/create_edge_lengths.py
+++ scripts/create_edge_lengths.py
@@ -56,16 +56,29 @@ parser = argparse.ArgumentParser(description='This will take the matrix made by 
 parser.add_argument('-e', '--edge_list', help='Input edge list file of the KEGG hierarchy', required=True)
 parser.add_argument('-d', '--distances', help='File containing all pairwise distances between KOs. Use sourmash '
                                               'compare', required=True)
-parser.add_argument('-o', '--out_dir', help='Output directory')
+parser.add_argument('-o', '--out_file', help='Output file name: edge list with lengths in the last column', required=True)
 parser.add_argument('-A', '--A_matrix', help='A matrix file created by graph_to_matrix.py', required=True)
 parser.add_argument('-b', '--brite_id', help='Brite ID of the KEGG hierarchy you want to focus on. Eg. ko00001',
                     required=True)
+parser.add_argument('-n', '--num_iter', help='Number of random selections on which to perform the NNLS', default=100)
+parser.add_argument('-f', '--factor', help='Selects <--factor>*(A.shape[1]) rows for which to do the NNLS', default=5)
+parser.add_argument('-r', '--reg_factor', help='Regularization factor for the NNLS', default=1)
+parser.add_argument('--force', help='Overwrite the output file if it exists', action='store_true')
 args = parser.parse_args()
 brite = args.brite_id
 edge_list = args.edge_list
-out_dir = args.out_dir
+out_file = args.out_file
 distances_file = args.distances
 A_matrix_file = args.A_matrix
+force = args.force
+num_iter = int(args.num_iter)
+factor = int(args.factor)
+reg_factor = float(args.reg_factor)
+if num_iter < 1:
+    raise ValueError('Number of iterations must be at least 1')
+if factor < 1:
+    raise ValueError('Factor must be at least 1')
+
 # check that the files exist
 if not exists(edge_list):
     raise FileNotFoundError(f"Could not find {edge_list}")
@@ -74,8 +87,8 @@ if not exists(distances_file):
 if not exists(f"{distances_file}.labels.txt"):
     raise FileNotFoundError(f"Could not find {distances_file}.labels.txt in the same directory as {distances_file}")
 distances_labels_file = f"{distances_file}.labels.txt"
-if not exists(out_dir):
-    os.mkdir(out_dir)
+if os.path.exists(out_file) and not force:
+    raise FileExistsError(f"{out_file} already exists. Please delete it or choose another name, or use --force.")
 if not exists(A_matrix_file):
     raise FileNotFoundError(f"Could not find {A_matrix_file}")
 try:
@@ -112,29 +125,27 @@ for ko1 in pairwise_dist_KOs:
 y = np.array(y)
 # by default, the values are: 0 = most dissimilar, 1 = most similar, so to convert to a distance, we subtract from 1
 y = 1 - y
-print(f"Shape of y: {y.shape}")
 # import the A matrix
 A = sparse.load_npz(A_matrix_file)
-print(f"Shape of A: {A.shape}")
-# solve the least squares problem, might have negatives
-#x, residuals, rank, s = np.linalg.lstsq(A, y, rcond=None)
-#x, istop, itn, r1norm, r2norm, anorm, acond = lsqr(A, y, show=True)[0:7]
-#print(f"istop: {istop}")
-#print(f"itn: {itn}")
-#print(f"r1norm: {r1norm}")
-#print(f"r2norm: {r2norm}")
-#print(f"anorm: {anorm}")
-#print(f"acond: {acond}")
-#x = lsqr(A, y, show=True)[0]
 
-# Try nnls on the dense matrix
-#x, rnorm = nnls(A.todense(), y)
-#print(f"nnls rnorm: {rnorm}")
+xs = []
+for i in range(num_iter):
+    # randomly select a subset of the rows of A
+    num_rows = int(factor * A.shape[1])
+    row_indices = np.random.choice(A.shape[0], num_rows, replace=False)
+    A_small = A[row_indices, :]
+    y_small = y[row_indices]
+    # append a row of 1's to A_small
+    A_small = sparse.vstack([reg_factor*A_small, sparse.csr_matrix(np.ones(A_small.shape[1]))])
+    # append a 0 to y_small
+    y_small = np.append(reg_factor*y_small, 0)
+    # Try lsq_linear
+    res = lsq_linear(A_small, y_small, bounds=(0, 1), verbose=2)
+    x = res.x
+    xs.append(x)
 
-# Try lsq_linear
-res = lsq_linear(A, y, bounds=(0,1), verbose=2)
-x = res.x
-
+# take the average of the solutions
+x = np.mean(xs, axis=0)
 
 # import the basis
 with open(basis_name, 'r') as f:
@@ -142,7 +153,6 @@ with open(basis_name, 'r') as f:
     basis = [line.strip() for line in basis]
 
 # import the edge list
-edge_list = os.path.join("test_data", "kegg_ko_edge_df.txt")
 df = pd.read_csv(edge_list, sep='\t', header=0)
 # add a new column for the edge lengths
 df['edge_length'] = np.nan
@@ -152,5 +162,5 @@ for i, tail in enumerate(basis):
 # remove all the rows that aren't in the basis, so only focusing on the subtree defined by the given brite
 df = df[df['child'].isin(basis)]
 # write the new edge list to file
-df.to_csv(os.path.join(out_dir, f"kegg_ko_edge_df_with_edge_lengths_{brite}_{os.path.basename(distances_file)}.txt"), sep='\t',
-          index=False)
+df.to_csv(out_file, sep='\t', index=False)
+
