f9bfcfe4990c8c04b7d725f6910aa73b58b27caa
dkoslicki <dmkoslicki@gmail.com>
2022-10-04 00:01:31 -0400

    typo #9

--- experiments/KOtree/get_data.sh
+++ experiments/KOtree/get_data.sh
@@ -5,5 +5,5 @@ thisDir=$(pwd)
 koSketchDir=/data/shared_data/KEGG_data/sourmash_sketches/output_KOs/KOs_sketched/
 edgeListDir=/data/shared_data/KEGG_KO_hierarchy/results/
 cp ${koSketchDir}/KOs_sketched_scaled_10_compare_5 .
-cp ${koSketchDir}/KOs_sketched_scaled_10_compare_5_labels.txt .
+cp ${koSketchDir}/KOs_sketched_scaled_10_compare_5.labels.txt .
 cp ${edgeListDir}/kegg_ko_edge_df.txt .
