e25c1a0b873cb20f6790aa1f2131c06923b136ce
dkoslicki <dmkoslicki@gmail.com>
2022-10-13 23:31:01 -0400

    add output dendrograms for #9, save dendro clusters in anticipation of #13

--- experiments/QIITA_study/plot_distances_dendro.py
+++ experiments/QIITA_study/plot_distances_dendro.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # explore the pairwise distances. This script is intended to be run interactively
+import sys
+
 import pandas as pd
 import numpy as np
 import os
 import matplotlib
 import matplotlib.pyplot as plt
-from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.cluster.hierarchy import linkage, dendrogram, leaves_list, fcluster
 from scipy.spatial.distance import squareform
 import argparse
+import json
 matplotlib.use('Agg')
 
 def argument_parser():
@@ -15,11 +18,21 @@ def argument_parser():
         description='Given a matrix of pairwise FunUnifrac distances along with metadata associated '
                     'with the study, this script will plot the distances in a dendrogram. It is assumed that the'
                     'metadata file has a column called "sample_name" which contains the sample names which correspond'
-                    'to the samples in the distance matrix. The distance matrix is assumed to be a numpy array')
+                    'to the samples in the distance matrix. The distance matrix is assumed to be a numpy array.'
+                    ''
+                    'This script will also return a JSON object with the following information: '
+                    'As we move a cutoff threshold from left to right (top to bottom) of the dendrogram,'
+                    'keep track of how the underlying samples cluster. Eg. just below the root, the dendrogram splits'
+                    'into 2 subtrees. Thus the values of this JSON object[2] will be two clusters of sample names'
+                    'corresponding to these two groups. ')
     parser.add_argument('-d', '--distances', help='File of the pairwise unifrac distances', required=True)
     parser.add_argument('-o', '--out_dir', help='Output directory', required=True)
     parser.add_argument('-m', '--metadata', help='Metadata file', required=True)
-    parser.add_argument('-k', '--key', help='Metadata key to use to label dendrogram leaves', required=False, default='disease_type')
+    parser.add_argument('-k', '--key', help='Metadata key to use to label dendrogram leaves', required=False,
+                        default='disease_type')
+    parser.add_argument('-s', '--split', help='The number of splits to retain (i.e. how far right to move the threshold'
+                                              'when saving the JSON recording of clusters).', required=False,
+                        default=5, type=int)
     return parser
 
 
@@ -29,6 +42,7 @@ def main():
     out_dir = args.out_dir
     meta_data_file = args.metadata
     key = args.key
+    split = args.split
     # check if files exist
     if not os.path.exists(pairwise_dists_file):
         raise FileNotFoundError(f"Could not find {pairwise_dists_file}")
@@ -52,13 +66,12 @@ def main():
     prefix = meta_data['sample_name'][0].split('.')[0]  # get the prefix of the sample names. eg "13984."
     prefix = prefix + "."
     sample_names_with_prefix = [prefix + x for x in sample_names]
-    print(f"sample_names_with_prefix: {sample_names_with_prefix}")
     # get the metadata for the samples
     #meta_data_of_dist = meta_data[meta_data['sample_name'].isin(sample_names_with_prefix)]
     # cluster the pairwise distances
     Z = linkage(pairwise_dists_cond, method='ward')
     # plot the dendrogram
-    plt.figure(figsize=(10, 40), dpi=600)
+    plt.figure(figsize=(20, 40), dpi=600)
     plt.title(f"Hierarchical Clustering of\n {pairwise_dists_file}")
     plt.xlabel('sample index')
     plt.ylabel('distance')
@@ -68,8 +81,37 @@ def main():
     dendrogram(Z, labels=labels, orientation='left', leaf_font_size=7)
     # make the labels bigger
     plt.rcParams.update({'font.size': 22})
-    plt.savefig(os.path.join(out_dir,f"dendro_{pairwise_dists_file}.png"), dpi=600)
+    plt.savefig(os.path.join(out_dir, f"dendro_{pairwise_dists_file}.png"), dpi=600)
+
+    # prepare the json dump of the clusters as we move the cutoff to the right
+    # These are the values on the x-axis of the dendrogram where clusters are merged
+    cuts = Z[:, 2]
+    # The midpoints between the cuts
+    cut_mids = (cuts[1:] + cuts[:-1]) / 2
+    # This is a dictionary going from the top/left of the dendrogram to the bottom/right
+    # i.e. most clustered to least clustered
+    cut_ind_to_mids = dict()
+    for i, cut in enumerate(cut_mids[::-1]):
+        cut_ind_to_mids[i] = cut
+
+    cut_to_clusters = dict()
+    for cut_ind in range(min(split, len(cut_mids))):
+        mid = cut_ind_to_mids[cut_ind]
+        cut_to_clusters[int(cut_ind)] = dict()
+        clust_inds = fcluster(Z, mid, criterion='distance')
+        clusts = dict()
+        for i, sample in zip(clust_inds, basis):
+            if i not in clusts:
+                clusts[int(i)] = []
+            clusts[int(i)].append(sample)
+        cut_to_clusters[int(cut_ind)] = clusts
+
+    # write the clusters to a json file
+    with open(os.path.join(out_dir, f"dendro_{pairwise_dists_file}_clusters.json"), 'w') as f:
+        json.dump(cut_to_clusters, f, indent=4)
 
 
 if __name__ == '__main__':
     main()
+
+
