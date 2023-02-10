a6beb31cae864d85064390301c14958db0995538
Adam Park <akp6031@psu.edu>
2023-02-10 15:49:08 -0500

    Split interface/business of scripts

--- scripts/create_edge_lengths.py
+++ scripts/create_edge_lengths.py
@@ -2,44 +2,16 @@
 import argparse
 import os
 import sys
-from os.path import exists
-import numpy as np
-import networkx as nx
-from scipy import sparse
-import pandas as pd
-from scipy.optimize import lsq_linear
-import multiprocessing
-from itertools import repeat
 # relative imports
 try:
     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(os.path.dirname(SCRIPT_DIR))
 except:
     pass
-from src.CONSTANTS import BRITES
-from src.LP_EMD_helper import get_descendants, get_descendant, get_KO_labels_and_index
+from src.commands import create_edge_lengths
 
 
-def map_func(itr, A, y, factor, reg_factor):
-    num_rows = int(factor * A.shape[1])
-    row_indices = np.random.choice(A.shape[0], num_rows, replace=False)
-    A_small = A[row_indices, :]
-    y_small = y[row_indices]
-    # append a row of 1's to A_small
-    A_small = sparse.vstack([reg_factor * A_small, sparse.csr_matrix(np.ones(A_small.shape[1]))])
-    # append a 0 to y_small
-    y_small = np.append(reg_factor * y_small, 0)
-    # Use lsq_linear to solve the NNLS problem
-    res = lsq_linear(A_small, y_small, bounds=(0, 1), verbose=2)
-    x = res.x
-    return x
-
-
-def map_star(args):
-    return map_func(*args)
-
-
-def argument_parser():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='This will take the matrix made by graph_to_path_matrix.py and the all '
                     'pairwise distance matrix and solve the least squares problem of '
@@ -61,99 +33,5 @@ def argument_parser():
     parser.add_argument('--distance',
                         help='Flag indicating that the input matrix is a distance (0=identical). If not, it is assumed to be a similarity (1=identical).',
                         action='store_true')
-    return parser
-
-def main():
-    # parse arguments
-    parser = argument_parser()
-    args = parser.parse_args()
-    brite = args.brite_id
-    edge_list = args.edge_list
-    out_file = args.out_file
-    distances_file = args.distances
-    A_matrix_file = args.A_matrix
-    force = args.force
-    num_iter = int(args.num_iter)
-    factor = int(args.factor)
-    reg_factor = float(args.reg_factor)
-    isdistance = args.distance
-    if num_iter < 1:
-        raise ValueError('Number of iterations must be at least 1')
-    if factor < 1:
-        raise ValueError('Factor must be at least 1')
-
-    # check that the files exist
-    if not exists(edge_list):
-        raise FileNotFoundError(f"Could not find {edge_list}")
-    if not exists(distances_file):
-        raise FileNotFoundError(f"Could not find {distances_file}")
-    if not exists(f"{distances_file}.labels.txt"):
-        raise FileNotFoundError(f"Could not find {distances_file}.labels.txt in the same directory as {distances_file}")
-    distances_labels_file = f"{distances_file}.labels.txt"
-    if os.path.exists(out_file) and not force:
-        raise FileExistsError(f"{out_file} already exists. Please delete it or choose another name, or use --force.")
-    if not exists(A_matrix_file):
-        raise FileNotFoundError(f"Could not find {A_matrix_file}")
-    try:
-        basis_name = f"{A_matrix_file.removesuffix('_A.npz')}_column_basis.txt"
-    except AttributeError:
-        if A_matrix_file.endswith('_A.npz'):
-            basis_name = f"{A_matrix_file[:-6]}_column_basis.txt"
-        else:
-            raise FileNotFoundError(f"Could not find the basis file that should accompany {A_matrix_file}. It appears the "
-                                    f"matrix file does not end in '_A.npz'. Was it created with graph_to_path_matrix.py?")
-    if not exists(basis_name):
-        raise FileNotFoundError(f"Could not find the basis file {basis_name} that should accompany {A_matrix_file}.")
-
-    if brite not in BRITES:
-        raise ValueError(f"{brite} is not a valid BRITE ID. Choices are: {BRITES}")
-
-
-    # import the basis of the A matrix (the shortest path matrix)
-    with open(basis_name, 'r') as f:
-        basis = f.readlines()
-        basis = [line.strip() for line in basis]
-
-    # import pairwise distances
-    pairwise_dist = np.load(distances_file)
-    # import label names
-    pairwise_dist_KOs, pairwise_dist_KO_index = get_KO_labels_and_index(distances_labels_file, basis=basis)
-
-    # create the y vector of all pairwise distances
-    y = []
-    for ko1 in pairwise_dist_KOs:
-        for ko2 in pairwise_dist_KOs:
-            y.append(pairwise_dist[pairwise_dist_KO_index[ko1], pairwise_dist_KO_index[ko2]])
-    y = np.array(y)
-    # by default, the values are: 0 = most dissimilar, 1 = most similar, so to convert to a distance, we subtract from 1
-    if not isdistance:
-        y = 1 - y
-    # import the A matrix
-    A = sparse.load_npz(A_matrix_file)
-    if A.shape[0] != y.shape[0]:
-        raise ValueError(f"The A matrix has {A.shape[0]} rows, but the y vector has {y.shape[0]} elements. "
-                         f"Something is wrong.")
-
-    num_threads = 1  # numpy apparently uses all PHYSICAL cores, twice that for hyperthreading
-    pool = multiprocessing.Pool(num_threads)
-
-    xs = np.array(pool.map(map_star, zip(range(num_iter), repeat(A), repeat(y), repeat(factor), repeat(reg_factor)), chunksize=num_iter // num_threads))
-
-    # take the average of the solutions
-    x = np.mean(xs, axis=0)
-
-    # import the edge list
-    df = pd.read_csv(edge_list, sep='\t', header=0)
-    # add a new column for the edge lengths
-    df['edge_length'] = np.nan
-    # iterate through the basis and add the edge lengths to the dataframe
-    for i, tail in enumerate(basis):
-        df.loc[df['child'] == tail, 'edge_length'] = x[i]
-    # remove all the rows that aren't in the basis, so only focusing on the subtree defined by the given brite
-    df = df[df['child'].isin(basis)]
-    # write the new edge list to file
-    df.to_csv(out_file, sep='\t', index=False)
-
-
-if __name__ == '__main__':
-    main()
+    # call main function
+    create_edge_lengths.main(parser.parse_args())
