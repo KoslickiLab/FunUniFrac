460b691e8a0f110f697080c2cdee14a5b3029d76
raquellewei <raquelle70679@gmail.com>
2023-04-27 11:42:17 -0400

    added postprocessing function and moved class to Kegg_tree

--- temp/extract_subtree.py
+++ temp/extract_subtree.py
@@ -1,47 +1,38 @@
-# Open input file and read edge list
-with open('../data/kegg_ko_edge_df_br_ko00001.txt_AAI_lengths_n_50_f_10_r_100.txt', 'r') as f:
-    edges = [tuple(map(str, line.split())) for line in f.readlines()]
+import networkx as nx
+from Kegg_tree import KeggTree, write_subgraph_file
+import random
 
-edges.remove(('#parent', 'child', 'edge_length'))
+#original tree
+edge_list = '../data/kegg_ko_edge_df_br_ko00001.txt_AAI_lengths_n_50_f_10_r_100.txt'
+G = nx.read_edgelist(edge_list, delimiter='\t', nodetype=str, create_using=nx.DiGraph, data=(('edge_length', float),))
 
-# Define a function to extract a subtree of the specified size
-def extract_subtree_of_size(size, edges):
-    def dfs(node, visited):
-        # Returns the size of the subtree rooted at the given node, and the edges in the subtree
-        size = 1
-        subtree = []
-        visited.add(node)
-        for edge in edges:
-            if edge[0] == node and edge[1] not in visited:
-                child_size, child_subtree = dfs(edge[1], visited)
-                size += child_size
-                if size == target_size:
-                    return size, subtree + [edge] + child_subtree
-                elif size > target_size:
-                    return size - child_size, subtree
-                subtree += [edge] + child_subtree
-        return size, subtree
+def extract_subtree_by_level_random(G, level):
+    kegg_tree = KeggTree(G)
+    kegg_tree.group_nodes_by_depth()
+    root = random.choice(kegg_tree.nodes_by_depth[level])
+    subtree_nodes = nx.descendants(G, root)
+    subtree_nodes.add(root)
+    sub_tree = G.subgraph(subtree_nodes)
+    return sub_tree
 
-    # Compute the target size of the output subtree
-    target_size = size
+def extract_subtree_by_root(G, root):
+    subtree_nodes = nx.descendants(G, root)
+    subtree_nodes.add(root)
+    sub_tree = G.subgraph(subtree_nodes)
+    return sub_tree
 
-    # Try extracting a subtree from each leaf node of the input tree
-    best_size = 0
-    best_subtree = None
-    for leaf in range(1, len(edges) + 2):
-        visited = set()
-        size, subtree = dfs(leaf, visited)
-        if size == target_size and (best_subtree is None or size > best_size):
-            best_size = size
-            best_subtree = subtree
+def perturb_edges(subgraph):
+    new_tree = nx.DiGraph()
+    for edge in subgraph.edges.data():
+        new_edge = edge[2]['edge_length'] + random.random()
+        new_tree.add_edge(edge[0], edge[1], edge_length=new_edge)
+    new_kegg_tree = KeggTree(new_tree)
+    new_kegg_tree.group_nodes_by_depth()
+    return new_tree
 
-    return best_subtree
 
-# Extract a subtree of the specified size
-subtree = extract_subtree_of_size(30, edges)
-print(subtree)
-
-# Write the subtree to an output file in edge list format
-with open('./sub_graph_size_30_chatgpt.txt', 'w+') as f:
-    for edge in subtree:
-        f.write(f'{edge[0]} {edge[1]}\n')
+sub_tree = extract_subtree_by_level_random(G, 2)
+new_tree = perturb_edges(sub_tree)
+outfile_name = 'subtree_size' + str(new_tree.size()) + '.txt'
+print(outfile_name)
+write_subgraph_file(new_tree, new_tree.nodes, outfile_name)
