a22ca17050762180dc2103b79fdccff1ef4d8972
raquellewei <raquelle70679@gmail.com>
2023-09-19 15:49:40 -0400

    complete extract_motif for all files

--- scripts/reproducibility/extract_motif.py
+++ scripts/reproducibility/extract_motif.py
@@ -1,13 +1,16 @@
 import gzip
 from line_profiler_pycharm import profile
 import json
+import pandas as pd
+from itertools import combinations
+
 
 data_dict = dict()
-#{KO:[list of motif]
+files = '/scratch/shared_data/KEGG_FTP/kegg/genes/organisms/*/*ent.gz'
+#{KO:[set of motifs]
 
 @profile
-def parse_file():
-    file = 'data/T03764.ent.gz'
+def parse_file(file):
     with gzip.open(file, 'rt') as f:
         data = f.read()
     entries = data.split('///')
@@ -20,12 +23,28 @@ def parse_file():
             if line.startswith('MOTIF'):
                 list_of_motifs = line.split()[2:]
         if KO in data_dict:
-            data_dict[KO] += list_of_motifs
+            data_dict[KO] += data_dict[KO].union(list_of_motifs)
         else:
-            data_dict[KO] = list_of_motifs
-    with open('data/test_motif_extract.json', 'w') as f:
-        json.dump(data_dict, f, indent=4)
+            data_dict[KO] = set(list_of_motifs)
 
 
-parse_file()
-print(len(data_dict))
+for file in files:
+    parse_file()
+
+with open('data/motifs_for_KO.json', 'w') as f:
+    json.dump(data_dict, f, indent=4)
+
+#compute pairwise distances using jaccard index
+def compute_jaccard(KO1, KO2):
+    jaccard = len(data_dict[KO1].intersection(data_dict[KO2]))/len(data_dict[KO1].union(data_dict[KO2]))
+    return jaccard
+
+KO_list = list(data_dict.keys())
+df = pd.DataFrame(index=KO_list, columns=KO_list)
+for KO in KO_list:
+    df[KO][KO] = 0.
+for (KO1, KO2) in combinations(KO_list, 2):
+    df[KO1][KO2] = df[KO2][KO1] = compute_jaccard(KO1, KO2)
+
+df.to_csv('data/pw_dist_by_motifs_all.csv')
+df.to_numpy('data/pw_dist_by_motifs_all.npy')
