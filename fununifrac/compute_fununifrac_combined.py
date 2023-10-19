36df7a451ece543abf113a771ede96e1508727bb
raquellewei <raquelle70679@gmail.com>
2023-10-19 14:18:43 -0400

    fix dtype

--- fununifrac/compute_fununifrac_combined.py
+++ fununifrac/compute_fununifrac_combined.py
@@ -1,16 +1,29 @@
-import os, sys, argparse
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(ROOT_DIR)
-import multiprocessing
+import argparse
 import logging
 import pandas as pd
 from src.objects.func_tree import FuncTreeEmduInput
 import src.factory.make_tree as make_tree
 import src.factory.make_emd_input as make_emd_input
-import src.utility.constant as constant
 from src.algorithms.emd_unifrac import EarthMoverDistanceUniFracSolver
-import src.utility.kegg_db as kegg_db
 import numpy as np
+
+
+def make_fununifrac_inputs(raw_P, input, normalize=True):
+    #convert an array into one that's suitable for use
+    EMDU_index_2_node = input.idx_to_node
+    node_2_EMDU_index = {v: k for k, v in EMDU_index_2_node.items()}
+
+    if normalize:
+        raw_P = raw_P/raw_P.sum()
+    P = np.zeros(len(EMDU_index_2_node))
+    for ko in raw_P.index:
+        if ko not in node_2_EMDU_index:
+            print(f"Warning: {ko} not found in EMDU index, skipping.")
+        else:
+            P_index = node_2_EMDU_index[ko]
+            P[P_index] = raw_P[ko]
+    return P
+
 
 def main():
     parser = argparse.ArgumentParser(description="This script will take a directory of sourmash gather results and "
@@ -27,8 +40,6 @@ def main():
     parser.add_argument('-a', '--abundance_key',
                         help='Key in the gather results to use for abundance. Default is `f_unique_weighted`',
                         default='f_unique_weighted')
-    parser.add_argument('-t', '--threads', help='Number of threads to use. Default is half the cores available.',
-                        default=int(multiprocessing.cpu_count() / 2), type=int)
     parser.add_argument('-b', '--brite', help='Use the subtree of the KEGG hierarchy rooted at the given BRITE ID. '
                                               'eg. brite:ko00001', default="ko00001", type=str, required=False)
     parser.add_argument('--diffab', action='store_true', help='Also return the difference abundance vectors.')
@@ -40,17 +51,15 @@ def main():
     parser.add_argument('--Ppushed', help="Flag indicating you want the pushed vectors to be saved.",
                         action="store_true")
 
-
     args = parser.parse_args()
     solver = EarthMoverDistanceUniFracSolver()
     # Parse the graph and get the FunUniFrac objects Tint, length, and edge_list
     tree = make_tree.import_graph(args.edge_list, directed=True)
     input: FuncTreeEmduInput = make_emd_input.tree_to_EMDU_input(tree, args.brite)
-    sample_df = pd.read_csv(args.file, dtype=float, index_col='name')
+    sample_df = pd.read_csv(args.file, index_col='name')
     Ps_pushed = {}
     for col in sample_df.columns:
-        P = sample_df[col].to_numpy()
-        P = P / P.sum()
+        P = make_fununifrac_inputs(sample_df[col], input)
         P_pushed = solver.push_up_L1(P, input)
         Ps_pushed[col] = P_pushed
     dists, diffabs_sparse = solver.pairwise_computation(Ps_pushed, sample_df.columns, input, args.diffab, args.L2)
