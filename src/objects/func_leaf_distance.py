4f5ad438cb0a783087d9d41ba26c1c199fd62432
raquellewei <raquelle70679@gmail.com>
2023-11-03 12:46:50 -0400

    debug

--- src/objects/func_leaf_distance.py
+++ src/objects/func_leaf_distance.py
@@ -36,7 +36,7 @@ class FuncTreeLeafPairwiseDistances:
                 print(indices)
                 print(type(indices[func1]), type(indices[func2]))
                 print(self.dists)
-                print(self.dists[0, 0])
+                print(self.dists[(0, 0)])
                 y.append(self.dists[indices[func1], indices[func2]])
         y = np.array(y)
         # by default, the values are: 0 = most dissimilar, 1 = most similar, so to convert to a distance, we subtract from 1
